eccbfb97d269cec07f722a73cd1001c566a7a942
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-10-22 11:32:52 +0800

    Added Tabbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,20 @@ import data from "../assets/data.json"
 import titleFormatter from "../helpers/titleFormatter";
 import {Sheet, Toolbar} from "konsta/react";
 import {Button} from "konsta/react";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {useRouter} from "next/router";
+import {CurrencyDollarIcon, HomeIcon, UserCircleIcon} from "@heroicons/react/24/outline";
+import {HomeIcon as HomeSolidIcon} from "@heroicons/react/24/solid";
+import {Popover} from "konsta/react";
+import {PlusIcon} from "@heroicons/react/24/solid";
 
 const DEFAULT_CENTER = [24.7972217, 120.9966699]
 
 export default function Home() {
     const [sheetOpened, setSheetOpened] = useState(false);
+    const [popoverOpened, setPopoverOpened] = useState(false);
     const [currentSheetId, setCurrentSheetId] = useState("1");
+    const popoverTargetRef = useRef()
     const router = useRouter()
     const openSheet = (id) => {
         setSheetOpened(true)
@@ -74,7 +80,7 @@ export default function Home() {
                 onBackdropClick={() => setSheetOpened(false)}
             >
                 <Toolbar top>
-                    <div className="left" />
+                    <div className="left"/>
                     <div className="right">
                         <Link toolbar onClick={() => setSheetOpened(false)}>
                             Done
@@ -86,10 +92,53 @@ export default function Home() {
                         商家名稱： {data.find((item) => item.id === currentSheetId)?.name}
                     </div>
                     <div className="mt-4">
-                        <Button onClick={() => router.push(`/store/${currentSheetId}`)} className={"w-full"}>查看更多</Button>
+                        <Button onClick={() => router.push(`/store/${currentSheetId}`)}
+                                className={"w-full"}>查看更多</Button>
                     </div>
                 </Block>
             </Sheet>
+
+
+            <Toolbar
+                top={false}
+                className={`left-0 bottom-0 fixed w-full py-2 px-4`}
+            >
+                <Popover
+                    opened={popoverOpened}
+                    target={popoverTargetRef.current}
+                    onBackdropClick={() => setPopoverOpened(false)}
+                >
+                    <List nested>
+                        <ListItem
+                            title="掃一掃"
+                            link
+                            onClick={() => setPopoverOpened(false)}
+                        />
+                        <ListItem
+                            title="我的條碼"
+                            link
+                            onClick={() => setPopoverOpened(false)}
+                        />
+                    </List>
+                </Popover>
+
+                <Link toolbar className={"flex flex-col items-center justify-center"} href={"/"}>
+                    {router.pathname === "/" ?
+                        <HomeSolidIcon className={"w-12 h-12"}/> :
+                        <HomeIcon className={"w-12 h-12"}/>
+                    }
+
+                    <div className={"text-sm"}>首頁</div>
+                </Link>
+                <Link toolbar className={"flex flex-col items-center justify-center"} ref={popoverTargetRef}>
+                    <CurrencyDollarIcon className={"w-12 h-12"} onClick={() => setPopoverOpened(true)}/>
+                    <div className={"text-sm"}>支付</div>
+                </Link>
+                <Link toolbar className={"flex flex-col items-center justify-center"}>
+                    <UserCircleIcon className={"w-12 h-12"}/>
+                    <div className={"text-sm"}>我的</div>
+                </Link>
+            </Toolbar>
         </Page>
     )
 }
