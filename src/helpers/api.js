6249d34a9487be1a2ad6deda3193e02509f5bcdc
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-10-22 21:22:25 +0800

    Fix

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const domain = process.env.NEXT_PUBLIC_API_ENDPOINT
-const token = localStorage.getItem('token')
 export const api = axios.create({
     baseURL: domain + '/api',
     headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
         'Content-Type': 'application/json',
         'Accept': "application/json"
     }
