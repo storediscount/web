73cd502b46371ffa1141585b54dac2daa46b9d25
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2022-10-22 23:03:48 +0800

    Build

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -4,7 +4,7 @@ const domain = process.env.NEXT_PUBLIC_API_ENDPOINT
 export const api = axios.create({
     baseURL: domain + '/api',
     headers: {
-        Authorization: `Bearer ${localStorage?.getItem('token')}`,
+        //Authorization: `Bearer ${localStorage?.getItem('token')}`,
         'Content-Type': 'application/json',
         'Accept': "application/json"
     }
