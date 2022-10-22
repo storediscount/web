1d07d6d0022a7128de65010cebaa939ea649385f
chyyen <jonason2004@gmail.com>
2022-10-22 22:31:59 +0800

    Add return function to detail page

--- src/pages/wallet/detail.jsx
+++ src/pages/wallet/detail.jsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import data from "../../assets/data.json"
-import {BlockTitle, Link, List, ListItem, Navbar, Page} from "konsta/react";
 import titleFormatter from "../../helpers/titleFormatter";
+import {ChevronLeftIcon} from "@heroicons/react/24/outline";
+import {BlockTitle, Link, List, ListItem, Navbar, Page} from "konsta/react";
 import {useRouter} from "next/router";
 
 const TransactionRecord = [
@@ -56,7 +57,10 @@ export default function WalletDetail() {
                 <title>{titleFormatter("錢包明細")}</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <Navbar title={titleFormatter("錢包明細")}/>
+            <Navbar 
+                title={titleFormatter("錢包明細")}
+                left={<Link onClick={() => router.back()} navbar><ChevronLeftIcon className={"h-4 w-4"}/>返回</Link>}
+            />
 
             <BlockTitle>錢包明細</BlockTitle>
             <List strongIos outlineIos>
