0c1e8e153f17a63ffd50a41433ab2671469e4c0a
chyyen <jonason2004@gmail.com>
2022-10-23 08:26:18 +0800

    Add coupon discount

--- src/pages/wallet/pay.jsx
+++ src/pages/wallet/pay.jsx
@@ -1,18 +1,21 @@
 import Head from "next/head";
-import {Block, BlockTitle, Button, Link, List, ListItem, Navbar, Page, Sheet, Toolbar} from "konsta/react";
+import {Block, BlockTitle, Button, Link, List, ListItem, Navbar, Page, Sheet, Toolbar, Checkbox} from "konsta/react";
 import Html5QrcodePlugin from "../../components/QRCodeScanner";
 import titleFormatter from "../../helpers/titleFormatter";
 import {useState} from "react";
 import stores from "../../assets/data.json";
+import coupons from "../../assets/coupon_ticket.json"
 import Swal from "sweetalert2";
 import {useRouter} from "next/router";
 
 export default function WalletDetail() {
     const [sheetOpened, setSheetOpened] = useState(false);
     const [totalPrice, setTotalPrice] = useState(0);
+    const [finalPrice, setFinalPrice] = useState(0);
     const [order, setOrder] = useState([]);
     const [store, setStore] = useState(1);
     const [scanned, setScanned] = useState(false);
+    const [checkedCoupon, setCheckedCoupon] = useState((new Array(coupons.length)).fill(false))
     const router = useRouter()
     const result = (res) => {
         const data = JSON.parse(res);
@@ -30,10 +33,28 @@ export default function WalletDetail() {
                 }
                 const items = stores.find((place) => place.id === store).items;
                 setTotalPrice(order.reduce((acc, cur) => acc + items.find(item => item.id === cur.id).price * cur.amount, 0));
+                setFinalPrice(order.reduce((acc, cur) => acc + items.find(item => item.id === cur.id).price * cur.amount, 0));
                 setSheetOpened(true);
                 break
         }
     }
+    const calcDiscount = (_checkedCoupon) => {
+        // value first, percentage second
+        let percentage = 1.0, value = 0
+        for(let i=0;i<coupons.length;i++){
+            if(_checkedCoupon[i]){
+                percentage *= parseFloat(coupons[i].discountPercentage)
+                value += parseInt(coupons[i].discountValue)
+            }
+        }
+        setFinalPrice(Math.floor(Math.max((totalPrice-value)*percentage),0))
+    }
+    const handleCheckedCouponChange = (e, id) =>{
+        let _checkedCoupon = [...checkedCoupon]
+        _checkedCoupon[id-1] = e.target.checked
+        setCheckedCoupon(_checkedCoupon)
+        calcDiscount(_checkedCoupon)
+    } 
     const pay = () => {
         setSheetOpened(false)
         Swal.fire({
@@ -86,8 +107,19 @@ export default function WalletDetail() {
                             )
                         })}
                     </div>
+                    <div className={"flex flex-col gap-1"}>
+                        {coupons.map((coupon) => (
+                            <div className={"flex flex-row justify-between"} key={coupon.id}>
+                                <div>{coupon.name}</div>
+                                <Checkbox
+                                    checked={checkedCoupon[parseInt(coupon.id)-1]}
+                                    onChange={(e) => handleCheckedCouponChange(e,parseInt(coupon.id))}
+                                />
+                            </div>
+                        ))}
+                    </div>
                     <div className="mt-4">
-                        <Button onClick={pay}>支付 ${totalPrice}</Button>
+                        <Button onClick={pay}>支付 ${finalPrice}</Button>
                     </div>
                 </Block>
             </Sheet>
