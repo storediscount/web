eade4892fe63cad3a48adbe4f784549828a7c7dd
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-03-11 01:45:31 +0800

    card list

--- src/pages/card/[id]/index.jsx
+++ src/pages/card/[id]/index.jsx
@@ -1,7 +1,25 @@
-export default function () {
-    return (
-        <div>
+import Head from "next/head";
+import titleFormatter from "../../../helpers/titleFormatter";
+import {Link, Navbar, Page} from "konsta/react";
+import {ChevronLeftIcon} from "@heroicons/react/24/outline";
+import {useRouter} from "next/router";
 
-        </div>
+export default function () {
+    const name = "米塔會員";
+    const router = useRouter();
+    return (
+        <Page>
+            <Head>
+                <title>{titleFormatter(name)}</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Navbar title={titleFormatter(name)}
+                    left={<Link onClick={() => router.back()} navbar><ChevronLeftIcon
+                        className={"h-4 w-4"}/>返回</Link>}/>
+
+            <div className={"bg-gray-200 p-4 mx-2 cursor-pointer"} onClick={()=>router.push('/card/1')}>
+                hi
+            </div>
+        </Page>
     )
 }
